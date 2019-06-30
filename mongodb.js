5e0075bf09c6fae1c47a8f936f3742b9a1b7c6cf
Nick Tsoukas <nick.tsoukas101@gmail.com>
2019-06-29 19:36:07 -0500

    Expoloring methods and wrapping in function

--- mongodb.js
+++ mongodb.js
@@ -7,6 +7,7 @@ db.collection('some-collection').insertOne()
 
 ========= Read Operations ================
 db.collection('some-collection').find({pass_options: 'here'});
+db.collection('some-collection).findOne({_id: new ObjectId("somecodehere")})
 
 what are object ids
 GUI id global unique identify 
@@ -24,27 +25,54 @@ This way we do not keep on hitting the api
 
 // note cursor >>> points to document/data 
 
-const url = 'mongodb://127.0.0.1:27017';
 const createUser = require('./utils/createUser');
-const connectionMessage = require('./utils/connectionMessage');
+const connectionMessage = require('./utils/connectionMessage'); // simply logs that the connection was successful 
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
 
-
-const { MongoClient, ObjectID } = require('mongodb');
+const url = 'mongodb://127.0.0.1:27017';
 const dbName = 'task-app';
 const client = new MongoClient(url, {
     useNewUrlParser: true
 });
+//  helpers ============
+// This will return an ... so you don't have to type out the new ObjectID every time and litter up the code 
+const id = (id) => {
+    return new ObjectID(id);
+}
+
+/**
+ * finds and then logs a user for the collection 
+ * @param {string} name 
+ */
+const user = function findUser(name, db) {
+    console.log(`Looking for ${name}...`);
+    db.collection('users').findOne({
+            name: name
+        })
+        .then((user) => {
+            if(!user){
+                console.log(`Could not find the user ${name}`);
+            }
+            console.log(`We Found the`, user);
+            return user;
+        })
+        .catch((err) => {
+            if(err) console.log(err);
+        })
+}
 
 client.connect((err) => {
     if (err) throw err;
     const db = client.db(dbName);
     connectionMessage();
-    // createUser({name: 'kevin', coder: true}, db);
 
-   db.collection('users').find({coder: true}).toArray((err, user) => {
-       console.log(user);
-   });
-
+    user('Nick',db);
    
+
+
+
 
 });
